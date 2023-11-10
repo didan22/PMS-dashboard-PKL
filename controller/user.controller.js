afec68d9bc32ce8425fd6e6132a734854b200ec6
Sofi Rahmatulloh <118430785+tikdivmu@users.noreply.github.com>
2023-11-10 09:32:47 +0700

    req.body query params

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,5 +1,7 @@
 import { UserModel } from "../models/user.model.js"
 import inputValidation from "../utils/inputValidation.js"
+import bcrypt from "bcrypt"
+import { Types } from "mongoose"
 
 const reqField = {
 	username: "Username",
@@ -17,16 +19,19 @@ export const addUser = async function (req, res) {
 		if (!inputStatus.valid) {
 			return res.status(400).json({
 				success: false,
-				message: inputStatus.errMessage,
+				message: `${inputStatus.errMessage}Harus Diisi !`,
 			})
 		}
+
+		const salt = 12
+		const hashedPsw = await bcrypt.hash(password, salt)
 
 		// Jika inputan user lengkap
 		const user = new UserModel({
 			username,
 			fullname,
 			email,
-			password,
+			password: hashedPsw,
 			role,
 		})
 
@@ -40,7 +45,85 @@ export const addUser = async function (req, res) {
 	} catch (error) {
 		return res.status(500).json({
 			success: false,
-			message: "Backend Server Error !",
+			message: error.message ?? "Backend Server Error !",
+		})
+	}
+}
+
+export const listUser = async function (req, res) {
+	try {
+		// Tangkap Get Data
+		let { page, limit, search } = req.query
+		let offset = 0
+		page = page ? parseInt(page) : 1
+		limit = limit ? parseInt(limit) : 10
+
+		if (parseInt(limit) > 0) {
+			offset = (parseInt(page) - 1) * limit
+		}
+
+		if (!search) search = ""
+		const find = {
+			$or: [
+				{ username: { $regex: ".*" + search + ".*", $options: "i" } },
+				{ fullname: { $regex: ".*" + search + ".*", $options: "i" } },
+			],
+		}
+
+		const docs = await UserModel.countDocuments(find)
+		const data = await UserModel.find(find).limit(limit).skip(offset)
+
+		return res.status(201).json({
+			success: true,
+			message: "Request Valid !",
+			totalData: docs,
+			totalPage: Math.ceil(docs / limit),
+			dataShownPerPage: limit,
+			data,
+		})
+	} catch (error) {
+		return res.status(500).json({
+			success: false,
+			message: error.message ?? "Backend Server Error !",
+		})
+	}
+}
+
+export const detailUser = async function (req, res) {
+	try {
+		// Tangkap Get Data
+		const { id } = req.params
+
+		if (!id) {
+			return res.status(400).json({
+				success: false,
+				message: "ID kosong !",
+			})
+		}
+		if (!Types.ObjectId.isValid(id.toString())) {
+			return res.status(400).json({
+				success: false,
+				message: "ID tidak valid !",
+			})
+		}
+
+		const data = await UserModel.findById(id)
+		if (!data) {
+			return res.status(400).json({
+				success: false,
+				message: "Data user tidak ditemukan !",
+			})
+		}
+
+		return res.status(201).json({
+			success: true,
+			message: "Request Valid !",
+			data,
+		})
+	} catch (error) {
+		return res.status(500).json({
+			success: false,
+			message: error.message ?? "Backend Server Error !",
 		})
 	}
 }
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,10 @@
 import express from "express"
-import { addUser } from "../controller/user.controller.js"
+import { addUser, detailUser, listUser } from "../controller/user.controller.js"
 
 const router = express.Router()
 
+router.get("/", listUser)
+router.get("/detail/:id", detailUser)
 router.post("/", addUser)
 
 export default router
